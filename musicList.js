1a91b8ce115db797e3e25878babd448675a2f126
Parth-R-Satra <f20231266@pilani.bits-pilani.ac.in>
2024-07-08 20:42:40 +0530

    Add files via upload

---
+++ musicList.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Kabhi Kabhi Aditi",
+        artist: "Rashid Ali",
+        img: "Img-1.jpg",
+        src: "song-1.mp3",
+    },
+    {
+        name: "Tera Hone Laga Hoon",
+        artist: "Atif Aslam",
+        img: "Img-2.jpg",
+        src: "song-2.mp3",
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "Img-3.jpg",
+        src: "song-3.mp3",
+    },
+    {
+        name: "Counting Stars",
+        artist: "OneRepublic",
+        img: "Img-4.jpg",
+        src: "song-4.mp3",
+    },
+    {
+        name: "Maan Meri Jaan",
+        artist: "King,Nick Jonas",
+        img: "Img-5.jpg",
+        src: "song-5.mp3",
+    }
+]
